ebd2ad26017c677a4864590cb5b12fd2241f8ac8
AasthaTank <asthatank@gmail.com>
2023-07-16 14:32:32 +0530

    Login System with php-adding different pages

---
+++ signup.php
@@ -0,0 +1,93 @@
+<?php 
+$showAlert = false;
+$showError = false;
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    include 'partials/_dbconnect.php';
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $cpassword = $_POST["cpassword"];
+    $exists = false;
+    if(($password == $cpassword) && $exists==false){
+        $sql = "INSERT INTO `users` ( `username`, `password`, `dt`) VALUES ('$username', '$password', current_timestamp())";
+        $result = mysqli_query($conn,$sql);
+        if ($result){
+            $showAlert = true;
+        }
+    }
+    else{
+        $showError = "Passwords do not match";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <title>Signup</title>
+</head>
+<body>
+    
+    <?php require 'partials/_nav.php' ?>
+      <?php 
+        if($showAlert){
+            echo ' <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Success!</strong> Your account is now created and you can login.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div> '; 
+            }
+        if($showError){
+            echo ' <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Error!</strong> '. $showError.'
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div> '; 
+            }
+        ?>
+
+        <div class="container my-4">
+            <h2 class="text-center">Signup to our website</h2>
+    
+            <form action="/loginsystem/signup.php" method="post" 
+            style="display:flex; flex-direction:column; align-items:center; 
+            ">
+                <div class="form-group col-md-7">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" id="username" aria-describedby="emailHelp" name="username">
+                    
+                </div>
+
+                <div class="form-group col-md-7">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password"  name="password">
+                </div>
+
+                <div class="form-group col-md-7">
+                    <label for="cpassword">Confirm Password</label>
+                    <input type="password" class="form-control" id="cpassword" name="cpassword" >
+                    <small id="emailHelp" class="form-text text-muted">(Make sure to type the same password)</small>
+                </div>
+
+                <!-- <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div> -->
+                <button type="submit" class="btn btn-primary ml-3">Signup</button>
+            </form>
+        </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
